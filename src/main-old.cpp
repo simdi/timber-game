6fba1881cd0a203e66a644a60ccc78895e5b2e25
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-03-15 21:23:25 +0100

    Added background to sprite.

---
+++ src/main-old.cpp
@@ -0,0 +1,39 @@
+// #include "Platform/PlatformHelper.hpp"
+
+// int main()
+// {
+// 	PlatformHelper platform;
+
+// 	sf::RenderWindow window;
+// 	// in Windows at least, this must be called before creating the window
+// 	// float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
+//   sf::VideoMode vm(1920, 1039);
+//   // sf::VideoMode vm(1920.0f * screenScalingFactor, 1039.0f * screenScalingFactor);
+// 	// Use the screenScalingFactor
+// 	window.create(vm, "SFML works!");
+// 	platform.setIcon(window.getSystemHandle());
+
+// 	sf::CircleShape shape(window.getSize().x / 2);
+// 	shape.setFillColor(sf::Color::White);
+
+// 	sf::Texture shapeTexture;
+// 	shapeTexture.loadFromFile("graphics/background.png");
+// 	shape.setTexture(&shapeTexture);
+
+// 	sf::Event event;
+
+// 	while (window.isOpen())
+// 	{
+// 		while (window.pollEvent(event))
+// 		{
+// 			if (event.type == sf::Event::Closed)
+// 				window.close();
+// 		}
+
+// 		window.clear();
+// 		window.draw(shape);
+// 		window.display();
+// 	}
+
+// 	return 0;
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,38 @@
 #include "Platform/PlatformHelper.hpp"
 
-int main()
-{
+int main() {
 	PlatformHelper platform;
-
-	sf::RenderWindow window;
+  sf::RenderWindow window;
+  // sf::VideoMode vm(900, 1020);
 	// in Windows at least, this must be called before creating the window
-	// float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
-  sf::VideoMode vm(1920, 1039);
-  // sf::VideoMode vm(1920.0f * screenScalingFactor, 1039.0f * screenScalingFactor);
-	// Use the screenScalingFactor
-	window.create(vm, "SFML works!");
+  float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
+  sf::VideoMode vm(1920.0f * screenScalingFactor, 1080.0f * screenScalingFactor);
+
+	window.create(vm, "Timber!!!");
 	platform.setIcon(window.getSystemHandle());
 
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
+	sf::Texture textureBackground;
+  // Load graphics into texture.
+	textureBackground.loadFromFile("graphics/background.png");
 
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("graphics/background.png");
-	shape.setTexture(&shapeTexture);
+  // Create sprite
+  sf::Sprite spriteBackground;
+  // Attach the texture to the background
+  spriteBackground.setTexture(textureBackground);
+  // Set the sprite to cover the screen
+  spriteBackground.setPosition(0, 0);
 
 	sf::Event event;
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-
+    // Clear everything from the last frame
 		window.clear();
-		window.draw(shape);
+    // Draw the game scene.
+    window.draw(spriteBackground);
+    // Show everything we have drawn.
 		window.display();
 	}
 
